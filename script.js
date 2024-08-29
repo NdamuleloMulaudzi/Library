a3f65542f3fae9b146bb92167783272e9733319a
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-08-29 19:05:31 +0200

    added logic to display a book

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-const dispalyForm = document.querySelector("#displayFormBtn");
+const displayForm = document.querySelector("#displayFormBtn");
 const closeForm = document.querySelector("#closeFormBtn");
 const dialog = document.querySelector("dialog");
+const displayBooks = document.querySelector("#displayBook");
+
+//retrive input elements
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+const notRead = document.querySelector("#notRead");
 
 const myLibrary = [];
 
-function Book() {
-  this.info = function () {};
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
-dispalyForm.addEventListener("click", () => {
+function displayBook() {
+  displayBooks.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.innerHTML = `<h2>${book.title}</h2>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <p>Read: ${book.isRead ? "Yes" : "No"}</p>
+      <button id="remove">Remove</button>`;
+    displayBooks.appendChild(bookCard);
+  });
+}
+
+displayForm.addEventListener("click", () => {
   dialog.showModal();
 });
 
-closeForm.addEventListener("click", () => {
+closeForm.addEventListener("click", (event) => {
+  let book = new Book(title.value, author.value, pages.value);
+  addBookToLibrary(book);
+  displayBook();
   dialog.close();
+  event.preventDefault();
 });
