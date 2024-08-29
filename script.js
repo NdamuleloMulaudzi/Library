261d43e876f7d02d1db50ea3705228e2bbf99919
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-08-29 20:16:59 +0200

    completed logic for library

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const displayForm = document.querySelector("#displayFormBtn");
 const closeForm = document.querySelector("#closeFormBtn");
 const dialog = document.querySelector("dialog");
 const displayBooks = document.querySelector("#displayBook");
+const removeBook = document.querySelector("#remove");
 
 //retrive input elements
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
-const read = document.querySelector("#read");
-const notRead = document.querySelector("#notRead");
+const readStatus = document.querySelectorAll('input[name="readStatus"]');
 
 const myLibrary = [];
 
@@ -25,14 +25,30 @@ function addBookToLibrary(book) {
 
 function displayBook() {
   displayBooks.innerHTML = "";
-  myLibrary.forEach((book) => {
+  myLibrary.forEach((book, index) => {
     const bookCard = document.createElement("div");
     bookCard.innerHTML = `<h2>${book.title}</h2>
       <p>Author: ${book.author}</p>
       <p>Pages: ${book.pages}</p>
-      <p>Read: ${book.isRead ? "Yes" : "No"}</p>
-      <button id="remove">Remove</button>`;
+      <p>Read Yet?: ${book.isRead ? "Yes" : "No"} </p>
+      <button class="ReadBtn">${
+        book.isRead ? "I've Read" : "Not Yet Read"
+      }</button>
+      <button class="remove" id="${index}">Remove</button>`;
+
+    bookCard.querySelector(".ReadBtn").addEventListener("click", () => {
+      book.isRead = !book.isRead; // Toggle the read status
+      displayBook(); // Update the display to reflect changes
+    });
     displayBooks.appendChild(bookCard);
+  });
+
+  document.querySelectorAll(".remove").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const index = event.target.getAttribute("id");
+      myLibrary.splice(index, 1);
+      displayBook();
+    });
   });
 }
 
@@ -41,9 +57,10 @@ displayForm.addEventListener("click", () => {
 });
 
 closeForm.addEventListener("click", (event) => {
+  event.preventDefault();
+
   let book = new Book(title.value, author.value, pages.value);
   addBookToLibrary(book);
   displayBook();
   dialog.close();
-  event.preventDefault();
 });
